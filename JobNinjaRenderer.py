8609264c9bc6cffe081ed4d14e9dd6cbea95a587
Laurent Meyer <laurent.meyer@quatico.com>
2022-02-06 20:12:18 +0100

    F!!: make tests pass, add test case and implementation for the links

--- JobNinjaRenderer.py
+++ JobNinjaRenderer.py
@@ -1,5 +1,19 @@
+import re
+
 from mistletoe import HTMLRenderer
+
+email_replacement_regex = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
 
 
 class JobNinjaRenderer(HTMLRenderer):
-    pass
+
+    # Responsible for handling emails
+    def render_raw_text(self, token):
+        if '@' in token:
+            return email_replacement_regex.sub('<button class="email_replacement">Jetzt bewerben</button>',
+                                               token.content)
+        return token.content
+
+    # Responsible for handling links
+    def render_link(self, _):
+        return '<button class="email_replacement">Jetzt bewerben</button>'
--- new.specs.py
+++ new.specs.py
@@ -12,10 +12,13 @@ class NewProcessorTestCase(unittest.TestCase):
     def test_that_new_is_correct(self):
         for filename in all_test_markdowns:
             with open(filename) as file:
-                old_rendered_html = mistletoe.markdown(file.read(), JobNinjaRenderer)
+                new_rendered_html = mistletoe.markdown(file.read(), JobNinjaRenderer)
             with open(filename.replace(".md", ".html")) as html_file:
-                old_hmtl = html_file.read()
-        self.assertEqual(old_rendered_html, old_hmtl)
+                old_html = html_file.read()
+        print(old_html)
+        print("="*100)
+        print(new_rendered_html)
+        self.assertEqual(new_rendered_html.replace("\n", ""), old_html.replace("\n", ""))
 
 
 if __name__ == '__main__':
